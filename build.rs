138c55539a252c92df216c4ad5ccc5ec8155605d
Jan Arends <jan.arends@mailbox.org>
2026-04-22 09:18:20 +0200

    init

---
+++ build.rs
@@ -0,0 +1,23 @@
+use fst::Map;
+use std::path::Path;
+use std::{env, fs};
+
+include!("./icons/lookup_table.rs");
+
+fn main() {
+    let out_dir = env::var_os("OUT_DIR").unwrap();
+
+    let ext_icons_path = Path::new(&out_dir).join("ext_icons.bin");
+    let ext_icons = Map::from_iter(EXT_ICONS).unwrap();
+    fs::write(ext_icons_path, ext_icons.into_fst().as_bytes()).unwrap();
+
+    let filename_icons_path = Path::new(&out_dir).join("filename_icons.bin");
+    let filename_icons = Map::from_iter(FILENAME_ICONS).unwrap();
+    fs::write(filename_icons_path, filename_icons.into_fst().as_bytes()).unwrap();
+
+    let folder_icons_path = Path::new(&out_dir).join("folder_icons.bin");
+    let folder_icons = Map::from_iter(FOLDER_ICONS).unwrap();
+    fs::write(folder_icons_path, folder_icons.into_fst().as_bytes()).unwrap();
+
+    println!("cargo:rerun-if-changed=build.rs");
+}
---
+++ icons/README.md
@@ -0,0 +1,1 @@
+icon set generated and adapted from [vscode-icons](https://github.com/microsoft/vscode-icons) licensed under CC-BY-4.0.
